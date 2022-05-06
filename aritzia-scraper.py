49b417289ddb14b7330662bf062e39586d579802
emaiiii <airwickmai@gmail.com>
2022-05-05 23:27:05 -0700

    small adjustments to scraper

--- aritzia-scraper.py
+++ aritzia-scraper.py
@@ -1,22 +1,52 @@
+import time
 import requests
-from bs4 import BeautifulSoup
 import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
 
+PATH = 'C:\\Users\\Erik\\Documents\\Projects\\python\\aritzia-scraper\\chromedriver.exe'
 url = 'https://www.aritzia.com/us/en/sale?lastViewed=37'
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
+
+driver = webdriver.Chrome(PATH)
+driver.get(url)
+
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+while True:
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    # Wait to load page
+    time.sleep(4)
+
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight / 2);")
+
+    # Wait to load page
+    time.sleep(2)
+
+html = driver.page_source
+
+driver.quit()
+
+#response = requests.get(url)
+soup = BeautifulSoup(html, 'html.parser')
 
 clothing_items = soup.findAll('div', {'class':'product-info'})
 
 rows = []
 columns = ['product_brand', 'product_name', 'product_price', 'product_sale_price']
 
-
 for item in clothing_items:
-    product_brand = item.findAll('div', {'class' : 'product-brand'})[0].a.text
-    product_name = item.findAll('div', {'class' : 'product-name'})[0].a.text
-    product_price = item.findAll('div', {'class' : 'product-pricing'})[0].div.select('span')[0].text.strip()        
-    product_sale_price = item.findAll('div', {'class' : 'product-pricing'})[0].div.select('span')[2].text.replace('\n', '').replace('-', ' - ')
+    product_brand = str(item.findAll('div', {'class' : 'product-brand'})[0].a.text.strip())
+    product_name = str(item.findAll('div', {'class' : 'product-name'})[0].a.text.strip())
+    product_price = str(item.findAll('div', {'class' : 'product-pricing'})[0].select('span')[0].text.strip())
+    product_sale_price = str(item.findAll('div', {'class' : 'product-pricing'})[0].select('span')[2].text.strip())
 
     print('brand: ' + product_brand + '\n')
     print('name: ' + product_name + '\n')
@@ -25,7 +55,6 @@ for item in clothing_items:
 
     row = [product_brand, product_name, product_price, product_sale_price]
     rows.append(row)
-
 
 df = pd.DataFrame(rows, columns=columns)
 df.to_excel('aritzia_sale.xlsx', index=False)
